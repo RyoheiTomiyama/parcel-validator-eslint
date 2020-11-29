8d27e0330f6603f1ae7ae1af8fabf2866b7923ac
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2020-11-29 17:50:52 +0900

    fix rollup

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,13 +1,6 @@
-import buble from '@rollup/plugin-buble'
-
 export default {
   input: 'src/EslintValidator.js',
   output: [
-    {
-      file: 'dist/EslintValidator.umd.js',
-      format: 'umd',
-      name: 'EslintValidator',
-    },
     {
       file: 'dist/EslintValidator.esm.js',
       format: 'es',
@@ -16,8 +9,5 @@ export default {
       file: 'dist/EslintValidator.cjs.js',
       format: 'cjs',
     },
-  ],
-  plugins: [
-    buble(),
   ],
 }
